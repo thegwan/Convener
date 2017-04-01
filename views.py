0e7ee672e59d2e0337e7f62b758ae7a24b36d2e2
gwan <gwan@gwan-VirtualBox.(none)>
2017-04-01 10:31:13 -0400

    calendar placeholder template

--- views.py
+++ views.py
@@ -1,8 +1,10 @@
 # views.py
 
+from flask import request, render_template
 from main import app
 
 # connect the index webpage
-@app.route('/')
+@app.route("/", methods=['GET', 'POST'])
 def index():
-	return 'This is the homepage'
+	#return "This is the homepage. Method used: %s" % request.method
+	return render_template("calendar.html")
