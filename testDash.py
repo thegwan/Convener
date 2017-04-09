af89d27fb8c3cb551403a3f0c48b7a30c1ba3bf8
gwan <gwan.email@gmail.com>
2017-04-09 01:55:54 -0400

    server to database communication, updating db with meeting responses and creations

--- testDash.py
+++ testDash.py
@@ -3,14 +3,37 @@
 # Testing database server connection and initial protocol
 # data creation (run after creating an empty db)
 
-import dash, database, json
+import db2server, json
+import database as dbase
 
-print json.dumps(dash.toJSON("hsolis"), indent=2, sort_keys=True)
-print json.dumps(dash.toJSON("gwan"), indent=2, sort_keys=True)
-print json.dumps(dash.toJSON("ksha"), indent=2, sort_keys=True)
-print json.dumps(dash.toJSON("jackvw"), indent=2, sort_keys=True)
-print json.dumps(dash.toJSON("bargotta"), indent=2, sort_keys=True)
-print json.dumps(dash.toJSON("kl9"), indent=2, sort_keys=True)
+#-----------------------ok----------------------------
+
+# netid = "hsolis"
+# my_meetings = dbase.getUserCreatedMeetings(netid)
+# my_requests = dbase.getUserRequestedMeetings(netid)
+
+# #meetings = [meeting.mid for meeting in dbase.getUserMeetings(netid)]
+# meetings = dbase.getUserMeetings(netid)
+# confirmed = [m for m in meetings if m.allResponded]
+# pending   = [m for m in meetings if not m.allResponded]
+
+# assert(sorted([m.mid for m in my_meetings]) == sorted([1,2]))
+# assert(sorted([m.mid for m in my_requests]) == sorted([4]))
+# assert(sorted([m.creatorId for m in my_requests]) == "kl9")
+# print my_meetings
+# print my_requests
+# print confirmed
+# print pending
+
+#-----------------------ok----------------------------
+
+
+print json.dumps(db2server.init_protocol("hsolis"), indent=2, sort_keys=True)
+#print json.dumps(dash.init_protocol("gwan"), indent=2, sort_keys=True)
+#print json.dumps(dash.init_protocol("ksha"), indent=2, sort_keys=True)
+#print json.dumps(dash.init_protocol("jackvw"), indent=2, sort_keys=True)
+#print json.dumps(dash.init_protocol("bargotta"), indent=2, sort_keys=True)
+#print json.dumps(dash.init_protocol("kl9"), indent=2, sort_keys=True)
 
 
 #-----------------------------------------------------------------------
