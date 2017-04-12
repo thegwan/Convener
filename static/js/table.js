169a9bcab2d47d0061ca69286f6281f126b22add
Hector Solis <prestige_hxs@yahoo.com>
2017-04-11 22:23:54 -0400

    Heavy work done on the frontend. Wrote scripts that will display the meeting information from the init_data. Changed some styling so that the table will be disabled when pending meetings are clicked and enabled when requested are clicked.

--- static/js/table.js
+++ static/js/table.js
@@ -81,13 +81,48 @@ function clearSelected() {
 	}
 }
 
+// Make all the cells unselectable
+function makeUnselectable() {
+	var cells = document.getElementsByClassName('cell');
+	for (var i = 0; i < cells.length; i++) {
+  		// cells[i].classList.remove("selected");
+  		// cells[i].classList.remove("badtime");
+  		$(cells[i]).addClass('unselectable');
+	}
+	document.getElementById('mainTable').style.cursor = 'not-allowed';
+}
+
+// Make all the cells selectable again
+function makeSelectable() {
+	var cells = document.getElementsByClassName('cell');
+	for (var i = 0; i < cells.length; i++) {
+  		// cells[i].classList.remove("selected");
+  		// cells[i].classList.remove("badtime");
+  		cells[i].classList.remove('unselectable');
+	}
+	document.getElementById('mainTable').style.cursor = 'pointer';
+}
+
+// Removes everything from the table, makes the headers what they were on the start screen
+// Makes the clear and submit buttons visible again
+function resetEverything() {
+	makeSelectable();
+	clearSelected();
+	document.getElementById('tableHeader').innerText = 'Convener';
+	document.getElementById('getselected').style.visibility = 'visible';
+	document.getElementById('clearselected').style.visibility = 'visible';
+	document.getElementById('tableSubHeader').innerText = '';
+}
+
+// Takes the initial meeting JSON sent by the server and parses it into 
+// meetings that can be displayed on the page
 function parseInitialData(init_data) {
 	parsedData = JSON.parse(init_data);
 	
 	// For displaying the information from my_meetings
 	for (var i = 0; i < parsedData['my_meetings'].length; i++){
 		var meeting = parsedData['my_meetings'][i];
-		var div = document.createElement("DIV");
+		// Create a string of who has and hasn't responded
 		respString = '';
 		notRespString = '';
 		for (var j = 0; j < meeting['resp_netids'].length; j++){
@@ -96,26 +131,58 @@ function parseInitialData(init_data) {
 		for (var k = 0; k < meeting['nresp_netids'].length; k++){
 			notRespString += meeting['nresp_netids'][k] + ' ';
 		}
-		$(div).attr('tooltip', "Responded: " + respString + "Not Responded: " + notRespString);
+		// Creating a div and anchor element to display myMeeting
+		var div = document.createElement("DIV");
+		// Add a tooltip for when the meeting is hovered over
+		$(div).attr('tooltip', "Responded: " + respString + "\n" + " Not Responded: " + notRespString);
+		$(div).addClass('tooltipDiv');
 		var anchor = document.createElement("A");
-		//$(anchor).attr('onclick', 'myMeetingClicked(anchor)');
-		var f = clickMeet(i, meeting['title']);
-		// anchor.addEventListener('click', function(i) {
-		// 	myMeetingClicked(parsedData['my_meetings'][i]['times']);
-		// 	alert('clicked?');
-		// }, false);
+
+		// Function that runs when any myMeeting is clicked
+		var f = clickMyMeeting(i, meeting['title']);
 		anchor.addEventListener('click', f);
+
 		var textNode = document.createTextNode(meeting['title']);
 		anchor.appendChild(textNode);
 		div.appendChild(anchor)
 		document.getElementById('myMeetingsDiv').appendChild(div);
 	}
 
-	// fromDaysToTable(parsedData['my_meetings'][1]['times']);
+	// For displaying the information from pending
+	for (var i = 0; i < parsedData['pending'].length; i++){
+		var meeting = parsedData['pending'][i];
+		var div = document.createElement("DIV");
+
+		var anchor = document.createElement("A");
+
+		var f = clickPending(i, meeting['title']);
+
+		anchor.addEventListener('click', f);
+		var textNode = document.createTextNode(meeting['title']);
+		anchor.appendChild(textNode);
+		div.appendChild(anchor)
+		document.getElementById('myPendingDiv').appendChild(div);
+	}
+
+	// For displaying the information from requested
+	for (var i = 0; i < parsedData['my_requests'].length; i++){
+		var meeting = parsedData['my_requests'][i];
+		var div = document.createElement("DIV");
+
+		var anchor = document.createElement("A");
+
+		var f = clickRequested(i, meeting['title'], meeting['creator']);
+
+		anchor.addEventListener('click', f);
+		var textNode = document.createTextNode(meeting['title']);
+		anchor.appendChild(textNode);
+		div.appendChild(anchor)
+		document.getElementById('myRequestedDiv').appendChild(div);
+	}
 }
 
 // Returns an anonymous function that is attached to each item in myMeetings
-function clickMeet(i, title) {
+function clickMyMeeting(i, title) {
 	return function() {
 		myMeetingClicked(parsedData['my_meetings'][i]['times']);
 		document.getElementById('tableHeader').innerText = title;
@@ -125,10 +192,44 @@ function clickMeet(i, title) {
 
 // When myMeeting is clicked, displays the times and days on the table
 function myMeetingClicked(meetingElement) {
-	// alert(parsedData['confirmed'][0]['creator']);
+	resetEverything();
 	fromDaysToTable(meetingElement);
 }
 
+// Returns an anonymous function that is attached to each item in pending
+function clickPending(i, title) {
+	return function() {
+		pendingClicked(parsedData['pending'][i]['times']);
+		document.getElementById('tableHeader').innerText = title;
+		document.getElementById('getselected').style.visibility = 'hidden';
+		document.getElementById('clearselected').style.visibility = 'hidden';
+		document.getElementById('tableSubHeader').innerText = '-Your Response';
+		makeUnselectable();
+	}
+};
+
+// When pending is clicked, displays the user's response times and days on the table
+function pendingClicked(meetingElement) {
+	resetEverything();
+	fromDaysToTable(meetingElement);
+}
+
+// Returns an anonymous function that is attached to each item in requested
+function clickRequested(i, title, creator) {
+	return function() {
+		requestedClicked(parsedData['my_requests'][i]['times']);
+		document.getElementById('tableHeader').innerText = title;
+		document.getElementById('getselected').style.visibility = 'hidden';
+		document.getElementById('clearselected').style.visibility = 'hidden';
+		document.getElementById('tableSubHeader').innerText = 'Created by: ' + creator;
+	}
+};
+
+// When pending is clicked, displays the user's response times and days on the table
+function requestedClicked(meetingElement) {
+	resetEverything();
+	// fromDaysToTable(meetingElement);
+}
 
 // Given a list of days and time dicts, changes the table on the main screen 
 function fromDaysToTable(listOfDaysAndTimes) {
